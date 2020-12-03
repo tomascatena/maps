12c6bce20cf9ec0a1d30d0e1fc60d27944195bde
Tomás Catena <tomascatena@gmail.com>
2020-12-03 23:09:54 +0100

    added files

---
+++ src/User.ts
@@ -0,0 +1,27 @@
+import faker from "faker";
+import { Mappable } from './CustomMap';
+
+export class User implements Mappable {
+	name: string;
+	location: {
+		lat: number;
+		lng: number;
+	};
+	color: string = 'red';
+
+	constructor() {
+		this.name = faker.name.findName();
+		this.location = {
+			lat: parseFloat(faker.address.latitude()),
+			lng: parseFloat(faker.address.longitude()),
+		};
+	}
+
+	markerContent(): string {
+		return `
+		<div>
+			<h1>User Name: ${this.name}</h1>
+		</div>
+		`;
+	}
+}
